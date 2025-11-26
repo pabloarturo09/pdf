5343745824a0c0ad5bd70c5314cfbfdbb444c133
Pablo Arturo <pabloarturojmzdlc2004@gmail.com>
2025-11-25 23:44:52 -0600

    upload

--- app.js
+++ app.js
@@ -9,7 +9,11 @@ app.use(morgan('combined'));
 app.use('/pdf', express.static(path.join(__dirname, 'pdfs')));
 
 app.get('/verify/AKMMF0985JMMFHFHDSSD', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join(__dirname, 'canche.html'));
+});
+
+app.get('/verify/KJNNDJURHFJDNDJFHFBD', (req, res) => {
+    res.sendFile(path.join(__dirname, 'saul.html'));
 });
 
 app.listen(PORT, () => {
